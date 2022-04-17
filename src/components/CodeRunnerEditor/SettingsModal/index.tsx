6abf4d2fd371efb801d12de88cbf8b044914369c
Oscar Gauss Carvajal Yucra <oscargausscarvajal@gmail.com>
2022-04-17 13:19:45 -0400

    0.0.26

--- src/components/CodeRunnerEditor/SettingsModal/index.tsx
+++ src/components/CodeRunnerEditor/SettingsModal/index.tsx
@@ -8,32 +8,34 @@ import { SettingsModalProps } from '../types';
 export const SettingsModal = ({ onClose, isOpen, onChange, theme, keyMap, tabSize }: SettingsModalProps) => {
   
   return (
-    <Modal className="editor-settings-modal" isOpen={isOpen} onClose={onClose} closeIcon>
-      <h6><T>code editor settings</T></h6>
-      <div className="editor-settings-body">
-        <div>
-          <label className="text-semi-bold text-sentence-case"><T>choose editor style</T>: </label>
-          <Select
-            options={CODE_EDITOR_THEMES.map(theme => ({ value: theme, label: theme }))}
-            optionSelected={{ label: theme, value: theme }}
-            onChange={({ value }) => onChange?.({ theme: value })}
-          />
-        </div>
-        <div>
-          <label className="text-semi-bold text-sentence-case"><T>choose your editor key mapping</T>: </label>
-          <Select
-            options={CODE_EDITOR_KEY_MAPS.map(keyMap => ({ value: keyMap, label: keyMap }))}
-            optionSelected={{ label: keyMap, value: keyMap }}
-            onChange={({ value }) => onChange?.({ keyMap: value })}
-          />
-        </div>
-        <div>
-          <label className="text-semi-bold text-sentence-case"><T>choose your tab size</T>: </label>
-          <Select
-            options={CODE_EDIT0R_TAB_SIZES.map(keyMap => ({ value: keyMap, label: keyMap + '' }))}
-            optionSelected={{ label: tabSize + '', value: tabSize }}
-            onChange={({ value }) => onChange?.({ tabSize: value })}
-          />
+    <Modal className="editor-settings-modal jk-pad" isOpen={isOpen} onClose={onClose} closeIcon>
+      <div className="jk-pad jk-col gap">
+        <h6><T>code editor settings</T></h6>
+        <div className="editor-settings-body jk-col filled gap">
+          <div className="jk-row start gap">
+            <label className="text-semi-bold text-sentence-case"><T>choose editor style</T>: </label>
+            <Select
+              options={CODE_EDITOR_THEMES.map(theme => ({ value: theme, label: theme }))}
+              optionSelected={{ label: theme, value: theme }}
+              onChange={({ value }) => onChange?.({ theme: value })}
+            />
+          </div>
+          <div className="jk-row start gap">
+            <label className="text-semi-bold text-sentence-case"><T>choose your editor key mapping</T>: </label>
+            <Select
+              options={CODE_EDITOR_KEY_MAPS.map(keyMap => ({ value: keyMap, label: keyMap }))}
+              optionSelected={{ label: keyMap, value: keyMap }}
+              onChange={({ value }) => onChange?.({ keyMap: value })}
+            />
+          </div>
+          <div className="jk-row start gap">
+            <label className="text-semi-bold text-sentence-case"><T>choose your tab size</T>: </label>
+            <Select
+              options={CODE_EDIT0R_TAB_SIZES.map(keyMap => ({ value: keyMap, label: keyMap + '' }))}
+              optionSelected={{ label: tabSize + '', value: tabSize }}
+              onChange={({ value }) => onChange?.({ tabSize: value })}
+            />
+          </div>
         </div>
       </div>
     </Modal>
