0dbc8ac7f9775bd3c7bc4968ec0c35327f1c7e1f
Oscar Gauss Carvajal Yucra <oscargausscarvajal@gmail.com>
2022-04-30 11:06:34 -0400

    v0.0.36

---
+++ src/components/graphics/icons/Basic/EnterFullScreen/index.tsx
@@ -0,0 +1,5 @@
+import { BasicIconProps } from '../../types';
+import { renderBasicIcon } from '../../utils';
+import Copy from './Copy';
+
+export const CopyIcon = (props: BasicIconProps) => renderBasicIcon(props, Copy);
---
+++ src/components/graphics/icons/Basic/ExitFullScreen/index.tsx
@@ -0,0 +1,5 @@
+import { BasicIconProps } from '../../types';
+import { renderBasicIcon } from '../../utils';
+import EnterFullScreen from './EnterFullScreen';
+
+export const EnterFullScreenIcon = (props: BasicIconProps) => renderBasicIcon(props, EnterFullScreen);
