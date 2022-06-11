223d9b1312de3974c7d89d51b7a573ee55d25492
Oscar Gauss Carvajal Yucra <oscargausscarvajal@gmail.com>
2022-06-11 12:38:57 -0400

    v0.0.73

---
+++ src/components/Input/SelectSearchable.tsx
@@ -0,0 +1,147 @@
+import React, { ReactNode, useCallback, useEffect, useRef } from 'react';
+import { useResizeDetector } from 'react-resize-detector';
+import { useVirtual } from 'react-virtual';
+import { classNames, getTextContent } from '../../helpers';
+import { useHandleState } from '../../hooks';
+import { CloseIcon, InputCheckbox, MultiSelectProps, Popover, SelectOptionType, UpIcon } from '../index';
+
+export const MultiSelect = <T, U extends ReactNode, >({
+  className,
+  options,
+  optionsSelected: initialOptionsSelected,
+  onChange,
+  showOptions: _showOptions,
+  onChangeShowOptions: _onChangeShowOptions,
+  disabled,
+  optionsPlacement = 'bottom',
+  block,
+  rowHeightOption = 32,
+  searchable = false,
+}: MultiSelectProps<T, U>) => {
+  
+  const { width: widthContainer, ref: selectLayoutRef } = useResizeDetector();
+  const [showOptions, setShowOptions] = useHandleState(false, _showOptions, _onChangeShowOptions);
+  
+  const selectedOptionRef = useRef<HTMLDivElement | null>(null);
+  
+  // useEffect(() => {
+  //   const timeout = setTimeout(() => {
+  //     if (showOptions && (optionRef.current?.scrollHeight || 0) > (optionRef.current?.clientHeight || 0)) {
+  //       selectedOptionRef.current?.scrollIntoView({ behavior: 'smooth', block: 'center' });
+  //     }
+  //   }, 100);
+  //   return () => {
+  //     clearTimeout(timeout);
+  //   };
+  // }, [showOptions]);
+  
+  const optionRef = useRef<HTMLDivElement | null>(null);
+  
+  const optionsSelected: SelectOptionType<T, U>[] = initialOptionsSelected.map(initialOptionSelected => ({
+    value: initialOptionSelected.value,
+    label: initialOptionSelected.label || options.find(option => option.value === initialOptionSelected.value)?.label || '' as unknown as U,
+  }));
+  
+  const widthLabels = Math.max(...[...options, ...optionsSelected].map(({ label }) => getTextContent(label).length));
+  
+  const isDisabled = disabled || !onChange;
+  const containerWidth = widthLabels * (12 + 5) + 35;
+  
+  const parentRef = useRef<HTMLDivElement>(null);
+  const { virtualItems, totalSize } = useVirtual({
+    size: options.length,
+    parentRef,
+    estimateSize: useCallback(() => rowHeightOption, [rowHeightOption]),
+    overscan: 5,
+  });
+  
+  const renderOption = (option: SelectOptionType<T, U>) => {
+    const selected = optionsSelected.some(optionSelected => JSON.stringify(option.value) === JSON.stringify(optionSelected.value));
+    const disabled = !!option.disabled;
+    return (
+      <div
+        className={classNames('jk-select-option', { selected, disabled })}
+        onClick={!option.disabled ? () => {
+          onChange?.(selected ? optionsSelected.filter(optionSelected => JSON.stringify(option.value) !== JSON.stringify(optionSelected.value)) : [
+            ...optionsSelected,
+            option,
+          ]);
+        } : undefined}
+        key={JSON.stringify(option.value)}
+        ref={(e) => {
+          if (optionsSelected.some(optionSelected => JSON.stringify(option.value) === JSON.stringify(optionSelected.value))) {
+            selectedOptionRef.current = e;
+          }
+        }}
+      >
+        <div className="jk-row gap left">
+          <InputCheckbox checked={selected} onChange={() => null} disabled={disabled} />
+          {option.label}
+        </div>
+      </div>
+    );
+  };
+  
+  return (
+    <Popover
+      triggerOn="click"
+      placement={optionsPlacement}
+      popoverClassName="jk-select-options-content"
+      visible={showOptions}
+      onVisibleChange={value => setShowOptions(value)}
+      content={
+        <div ref={optionRef} className={classNames('jk-select-options jk-border-radius-inline')}
+             style={{ width: block ? (widthContainer || 0) + 32 : containerWidth }}
+        >
+          <div ref={parentRef} className="">
+            <div className="" style={{ height: `${totalSize}px` }}>
+              {virtualItems.map(virtualRow => (
+                <div
+                  key={virtualRow.key}
+                  style={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    width: '100%',
+                    height: `${virtualRow.size}px`,
+                    transform: `translateY(${virtualRow.start}px)`,
+                  }}
+                  // className="jk-table-row"
+                >
+                  {renderOption(options[virtualRow.index])}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      }
+    >
+      <div
+        className={classNames('jk-multi-select-layout', className, { open: showOptions, disabled: isDisabled })}
+        style={{ width: block ? '100%' : `${containerWidth}px` }}
+      >
+        <div className="jk-select jk-border-radius-inline" ref={selectLayoutRef}>
+          <div className="jk-row jk-multi-select-selected-options">
+            {optionsSelected.map(optionSelected => (
+              <div className="jk-tag gray-6 jk-row" key={JSON.stringify(optionSelected.value)}>
+                {optionSelected.label}
+                {onChange && (
+                  <CloseIcon
+                    size="small"
+                    filledCircle
+                    onClick={event => {
+                      onChange(optionsSelected.filter(option => JSON.stringify(optionSelected.value) !== JSON.stringify(option.value)));
+                      event.stopPropagation();
+                    }}
+                    className="color-gray-3"
+                  />
+                )}
+              </div>
+            ))}
+          </div>
+          <UpIcon rotate={180} className="input-icon" />
+        </div>
+      </div>
+    </Popover>
+  );
+};
---
+++ src/components/graphics/icons/Basic/Search/index.tsx
@@ -0,0 +1,5 @@
+import { BasicIconProps } from '../../types';
+import { renderBasicIcon } from '../../utils';
+import Group from './Group';
+
+export const GroupIcon = (props: BasicIconProps) => renderBasicIcon(props, Group);
