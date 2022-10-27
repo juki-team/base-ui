36636aa53940bc107b0274623d8478e309627a4c
Oscar Gauss Carvajal Yucra <oscargausscarvajal@gmail.com>
2022-10-27 02:14:07 -0400

    v0.0.169

--- src/components/CodeViewer/imports.ts
+++
@@ -1,5 +0,0 @@
-import 'prismjs/components/prism-c';
-import 'prismjs/components/prism-cpp';
-import 'prismjs/components/prism-markdown';
-import 'prismjs/plugins/line-numbers/prism-line-numbers';
-import 'prismjs/plugins/show-language/prism-show-language';
