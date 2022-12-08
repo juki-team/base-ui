719d769eb30d13d0a08fca7ac66dfa8ae0140958
Oscar Gauss Carvajal Yucra <oscargausscarvajal@gmail.com>
2022-12-08 01:52:40 -0400

    v0.0.199

---
+++ src/components/graphics/icons/Basic/HeadsetMic/index.tsx
@@ -0,0 +1,5 @@
+import { BasicIconProps } from '../../types';
+import { renderBasicIcon } from '../../utils';
+import Bug from './Bug';
+
+export const BugIcon = (props: BasicIconProps) => renderBasicIcon(props, Bug);
