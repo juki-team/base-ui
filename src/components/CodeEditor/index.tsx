7923f2a82eb5026adf1825d20586bc808349f478
Oscar Gauss Carvajal Yucra <oscargausscarvajal@gmail.com>
2022-12-12 15:07:21 -0400

    0.0.209

--- src/components/CodeEditor/index.tsx
+++ src/components/CodeEditor/index.tsx
@@ -1,8 +1,11 @@
 // import CodeMirror from '@uiw/react-codemirror';
-import { PROGRAMMING_LANGUAGE } from '@juki-team/commons';
+import { cpp } from '@codemirror/lang-cpp';
+import CodeMirror from '@uiw/react-codemirror';
+// import { PROGRAMMING_LANGUAGE } from '@juki-team/commons';
 import React, { CSSProperties } from 'react';
-import ReactCodeMirror from './CodeMirror';
+// import ReactCodeMirror from './CodeMirror';
 import { CodeEditorKeyMap, CodeEditorProps, CodeEditorTheme } from './types';
+// import { javascript } from '@codemirror/lang-javascript';
 
 export const CodeEditor = ({
   sourceCode,
@@ -16,21 +19,31 @@ export const CodeEditor = ({
 }: CodeEditorProps) => {
   return (
     <div style={{ '--code-mirror-font-size': `${fontSize}px`, width: '100%', height: '100%' } as CSSProperties}>
-      <ReactCodeMirror
+      <CodeMirror
         value={sourceCode}
-        options={{
-          theme,
-          keyMap,
-          mode: PROGRAMMING_LANGUAGE[language]?.monacoKey,
-          smartIndent: true,
-          readOnly: readOnly || !onChange,
-          viewportMargin: Infinity,
-          tabSize,
-          indentUnit: tabSize,
-          indentWithTabs: false,
+        height="200px"
+        // extensions={[javascript({ jsx: true })]}
+        extensions={[cpp()]}
+        onChange={(...props) => {
+          console.log(props);
+          // onChange?.()
         }}
-        onChange={((instance: { getValue: () => string }) => onChange?.({ sourceCode: instance.getValue() }))}
       />
+      {/*<ReactCodeMirror*/}
+      {/*  value={sourceCode}*/}
+      {/*  options={{*/}
+      {/*    theme,*/}
+      {/*    keyMap,*/}
+      {/*    mode: PROGRAMMING_LANGUAGE[language]?.monacoKey,*/}
+      {/*    smartIndent: true,*/}
+      {/*    readOnly: readOnly || !onChange,*/}
+      {/*    viewportMargin: Infinity,*/}
+      {/*    tabSize,*/}
+      {/*    indentUnit: tabSize,*/}
+      {/*    indentWithTabs: false,*/}
+      {/*  }}*/}
+      {/*  onChange={((instance: { getValue: () => string }) => onChange?.({ sourceCode: instance.getValue() }))}*/}
+      {/*/>*/}
     </div>
   );
 };
