d8c93aa3ca4993c76936cbde58240b95b887c33a
Oscar Gauss Carvajal Yucra <oscargausscarvajal@gmail.com>
2023-01-22 11:43:33 -0400

    0.0.267

--- src/components/Menu/types.ts
+++ src/components/Menu/types.ts
@@ -9,25 +9,25 @@ export type MenuMobileSectionType = {
 
 type MenuType = { icon?: ReactNodeOrFunctionType, label: ReactNodeOrFunctionType, selected: boolean, onClick?: (isOpen?: boolean) => void, menuItemWrapper?: ReactNodeOrFunctionP1Type<ReactNode> };
 
-export interface HorizontalMenuProps {
+export interface MenuMobileProps {
+  rightMobile?: MenuMobileSectionType,
+  drawerMenuMobile?: ReactNodeOrFunctionP1Type<{ close: () => void, menu: MenuType[] }>,
+  centerMobile?: MenuMobileSectionType,
+}
+
+export interface HorizontalMenuProps extends MenuMobileProps {
   className?: string,
   // left?: ReactNodeOrFunctionP1Type<{ isOpen: boolean }>,
   // right?: ReactNodeOrFunctionP1Type<{ isOpen: boolean }>,
   leftSection?: ReactNodeOrFunctionType,
   rightSection?: ReactNodeOrFunctionType,
   menu: MenuType[],
-  rightMobile?: MenuMobileSectionType,
-  drawerMenuMobile?: ReactNodeOrFunctionP1Type<{ close: () => void, menu: MenuType[] }>,
-  centerMobile?: MenuMobileSectionType,
 }
 
-export interface VerticalMenuProps {
+export interface VerticalMenuProps extends MenuMobileProps {
   isOpen?: boolean,
   onToggle?: (isOpen: boolean) => void,
   bottomSection?: ReactNodeOrFunctionP1Type<{ isOpen: boolean }>,
   topSection?: ReactNodeOrFunctionP1Type<{ isOpen: boolean }>,
   menu: { icon: ReactNodeOrFunctionType, label: ReactNodeOrFunctionType, selected: boolean, onClick?: (isOpen?: boolean) => void, menuItemWrapper?: ReactNodeOrFunctionP1Type<ReactNode> }[],
-  rightMobile?: MenuMobileSectionType,
-  drawerMenuMobile?: ReactNodeOrFunctionType,
-  centerMobile?: MenuMobileSectionType,
 }
