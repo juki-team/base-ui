842ea583af1ba467d0bee8bc5e94e430f63e015a
Oscar Gauss Carvajal Yucra <oscargausscarvajal@gmail.com>
2023-02-27 08:41:27 -0400

    0.0.351

--- src/integrated-components/index.tsx
+++ src/integrated-components/index.tsx
@@ -1,5 +1,6 @@
 export * from './CodeRunnerEditor';
 export * from './ForgotPassword';
+export * from './HelpSection';
 export * from './Layouts';
 export * from './Login';
 export * from './ProblemSelector';
