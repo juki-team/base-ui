53435cc736da762ff989f56ac11c458e1293d950
OscarGauss <me@oscargauss.com>
2023-06-11 01:29:03 -0400

    Fixing UserPreviewModal

--- src/integrated-components/UserPreviewModal/index.tsx
+++ src/integrated-components/UserPreviewModal/index.tsx
@@ -1,9 +1,11 @@
 import { ContentResponseType, Status, UserBasicResponseDTO } from '@juki-team/commons';
 import React from 'react';
+import CopyToClipboard from 'react-copy-to-clipboard';
 import {
   BasicModalProps,
   Button,
   ButtonLoader,
+  ContentCopyIcon,
   FetcherLayer,
   LocationOnIcon,
   MailIcon,
@@ -34,7 +36,7 @@ export const UserPreviewModal = ({ nickname, onClose, userHref }: UserPreviewMod
     >
       <FetcherLayer<ContentResponseType<UserBasicResponseDTO>>
         url={settings.getAPI().user.summary({ params: { nickname } }).url}
-        onError={(error) => onClose(() => () => Status.ERROR, [Status.ERROR, 0], { fetcherLayerErrorEvent: error })}
+        onError={(error) => onClose(() => () => Status.ERROR, [ Status.ERROR, 0 ], { fetcherLayerErrorEvent: error })}
       >
         {({ data }) => (
           <div className="jk-pad-md jk-col stretch gap">
@@ -47,7 +49,10 @@ export const UserPreviewModal = ({ nickname, onClose, userHref }: UserPreviewMod
                 width={100}
               />
               <div className="jk-col gap stretch">
-                <h3>{data?.content?.nickname}</h3>
+                <div className="jk-row left gap nowrap">
+                  <h3 className="fl-tt-il">{data?.content?.nickname}</h3>
+                  <CopyToClipboard text={data?.content?.nickname}><ContentCopyIcon /></CopyToClipboard>
+                </div>
                 <div className="cr-g3">{data?.content?.givenName} {data?.content?.familyName}</div>
                 <div className="jk-divider tiny" />
                 {(data?.content?.city?.trim() || data?.content?.country?.trim()) && (
@@ -62,7 +67,11 @@ export const UserPreviewModal = ({ nickname, onClose, userHref }: UserPreviewMod
               </div>
             </div>
             <div
-              className={classNames('gap block stretch', { 'jk-col': viewPortSize === 'sm', 'jk-row': viewPortSize !== 'sm' })}>
+              className={classNames('gap block stretch', {
+                'jk-col': viewPortSize === 'sm',
+                'jk-row': viewPortSize !== 'sm',
+              })}
+            >
               <ButtonLoader size="small" type="light" onClick={onClose}><T>close</T></ButtonLoader>
               <a href={userHref} target="_blank" rel="noreferrer">
                 <Button size="small" extend>
