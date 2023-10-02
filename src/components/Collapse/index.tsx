351bea7bc8dd4c45909a64197aa0bb8b92c39c8e
Oscar Gauss Carvajal Yucra <me@oscargauss>
2023-10-02 04:04:34 -0400

    Fixing collapse

--- src/components/Collapse/index.tsx
+++ src/components/Collapse/index.tsx
@@ -1,10 +1,39 @@
 import React, { PropsWithChildren } from 'react';
+import { Collapse as ReactCollapse } from 'react-collapse';
 import { useResizeDetector } from 'react-resize-detector';
 import { classNames, renderReactNodeOrFunctionP1 } from '../../helpers';
 import { useHandleState } from '../../hooks';
 import { CollapseProps } from './types';
 
 export const Collapse = (props: PropsWithChildren<CollapseProps>) => {
+  
+  const {
+    children,
+    header,
+    className,
+    showContent: _showContent,
+    onChangeShowContent: _onChangeShowContent,
+  } = props;
+  
+  const [ isOpen, setIsOpen ] = useHandleState(false, _showContent, _onChangeShowContent);
+  
+  const open = () => setIsOpen(true);
+  const close = () => setIsOpen(false);
+  const toggle = () => setIsOpen(prevState => !prevState);
+  
+  return (
+    <div className={classNames('jk-collapse-container jk-col stretch', className, { collapsed: !isOpen })}>
+      {renderReactNodeOrFunctionP1(header, { isOpen, close, open, toggle })}
+      <ReactCollapse isOpened={isOpen}>
+        <div className="jk-collapse-collapsible- jk-col block stretch">
+          {renderReactNodeOrFunctionP1(children, { isOpen, close, open })}
+        </div>
+      </ReactCollapse>
+    </div>
+  );
+};
+
+export const CollapseOld = (props: PropsWithChildren<CollapseProps>) => {
   
   const {
     children,
