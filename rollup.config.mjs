8158b4af96bbe44d8fcfcc7455ebc7fc26c44b27
OscarGauss <me@oscargauss.com>
2023-11-17 18:10:24 -0400

    testing rollup 2

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,5 +1,5 @@
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import resolve from '@rollup/plugin-node-resolve';
+import resolve, { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import copy from 'rollup-plugin-copy';
@@ -31,6 +31,7 @@ export default {
   plugins: [
     peerDepsExternal(),
     resolve(),
+    nodeResolve(),
     commonjs(),
     typescript({ useTsconfigDeclarationDir: true }),
     copy({
