714fe8f98eaeda213563d9964a33662c741d7cfd
OscarGauss <me@oscargauss.com>
2023-12-10 19:12:31 -0400

    Fixing JukiTProvider

--- src/contexts/JukiTProvider/JukiTProvider.tsx
+++ src/contexts/JukiTProvider/JukiTProvider.tsx
@@ -1,5 +1,5 @@
-import { Language, ProfileSetting } from '@juki-team/commons';
-import React, { PropsWithChildren, useEffect } from 'react';
+import { consoleInfo, Language, ProfileSetting } from '@juki-team/commons';
+import React, { PropsWithChildren, useEffect, useState } from 'react';
 import { useJukiUser } from '../../hooks';
 import { TContext } from './context';
 import { JukiTProviderProps } from './types';
@@ -7,11 +7,17 @@ import { JukiTProviderProps } from './types';
 export const JukiTProvider = ({ i18n, children }: PropsWithChildren<JukiTProviderProps>) => {
   
   const { user } = useJukiUser();
+  const [ _, setTrigger ] = useState(Date.now());
   
   const userLanguage = user.settings?.[ProfileSetting.LANGUAGE] === Language.ES ? Language.ES : Language.EN;
   
   useEffect(() => {
+    i18n.on('languageChanged', () => setTrigger(Date.now));
+  }, []);
+  
+  useEffect(() => {
     void i18n.changeLanguage(userLanguage);
+    consoleInfo(`language changed to ${userLanguage}`);
   }, [ userLanguage ]);
   
   return (
