f4fb5a0895decf130c823a6d467b5dc34cb2195a
OscarGauss <me@oscargauss.com>
2023-12-10 00:58:21 -0400

    Testing rollup try 5

--- src/contexts/JukiProviders.tsx
+++ src/contexts/JukiProviders.tsx
@@ -3,7 +3,7 @@ import { ParsedUrlQuery } from 'querystring';
 import React, { PropsWithChildren } from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
-import { I18nextProvider } from 'react-i18next';
+// import { I18nextProvider } from 'react-i18next';
 import { JukiPageProvider } from './JukiPageProvider';
 import {
   AppendSearchParamsType,
@@ -55,7 +55,7 @@ export const JukiProviders = (props: PropsWithChildren<JukiProvidersProps>) => {
     tokenName,
     components,
     router,
-    i18n,
+    // i18n,
   } = props;
   
   return (
@@ -82,13 +82,14 @@ export const JukiProviders = (props: PropsWithChildren<JukiProvidersProps>) => {
             components={components}
           >
             <DndProvider backend={HTML5Backend}>
-              {i18n ? (
-                <I18nextProvider i18n={i18n}>
-                  {children}
-                </I18nextProvider>
-              ) : (
-                children
-              )}
+              {/*{i18n ? (*/}
+              {/*  <I18nextProvider i18n={i18n}>*/}
+              {/*    {children}*/}
+              {/*  </I18nextProvider>*/}
+              {/*) : (*/}
+              {/*  children*/}
+              {/*)}*/}
+              {children}
             </DndProvider>
           </JukiUIProvider>
         </JukiUserProvider>
