68cd237c840aae2d2813775184e5916d480d9229
OscarGauss <me@oscargauss.com>
2023-12-22 17:59:18 -0400

    Fixing welcome message

--- src/components/templates/MainMenu/WelcomeModal/index.tsx
+++ src/components/templates/MainMenu/WelcomeModal/index.tsx
@@ -17,13 +17,13 @@ export const WelcomeModal = ({ isOpen, nickname, onClose, onSeeMyProfile }: Welc
     <div className="jk-pad-md jk-row nowrap">
       <div>
         <h2><T>hi</T>&nbsp;<span className="given-name">{nickname}</span>!</h2>
-        <h3><T>welcome to the Online Juki Judge</T></h3>
+        <h3><T>welcome to the platform!</T></h3>
         <div className="jk-col gap stretch">
-          <p>
-            <T className="tt-se">
-              participate in coding contests ranging from beginner level to week-long coding marathons
-            </T>
-          </p>
+          {/*<p>*/}
+          {/*  <T className="tt-se">*/}
+          {/*    participate in coding contests ranging from beginner level to week-long coding marathons*/}
+          {/*  </T>*/}
+          {/*</p>*/}
           <div className="jk-row-col gap block">
             <ButtonLoader type="light" onClick={onSeeMyProfile} extend>
               <T className="ws-np">see my profile</T>
