09aa7625bc3a6e721c91bac787d4e8e655326567
OscarGauss <me@oscargauss.com>
2024-01-07 09:28:15 -0400

    Fixing LastPath hooks

--- src/hooks/useLastPath.ts
+++ src/hooks/useLastPath.ts
@@ -1,15 +1,35 @@
-import { useContext, useEffect } from 'react';
+import { consoleWarn } from '@juki-team/commons';
+import { createContext, useContext, useEffect } from 'react';
 import { LastPathContextRef } from '../contexts/JukiLastPathProvider/context';
+import { LastPathContextInterface, LastPathType } from '../contexts/JukiLastPathProvider/types';
 import { useJukiRouter } from './useJukiRouter';
 
-export const useLastPath = () => {
-  const { lastPath, pushPath } = useContext(LastPathContextRef.current!);
+const defaultContext = <T extends string | number, >() => createContext<LastPathContextInterface<T>>({
+  pushPath: () => null,
+  lastPath: {} as LastPathType<T>,
+});
+
+export const useLastPath = <T extends string | number, >() => {
+  
+  if (!LastPathContextRef.current) {
+    consoleWarn('last path context not initialized');
+  }
+  
+  const { lastPath, pushPath } = useContext(LastPathContextRef.current || defaultContext());
+  
   return { lastPath, pushPath };
 };
 
 export const useTrackLastPath = (key: string) => {
-  const { pushPath } = useContext(LastPathContextRef.current!);
+  
+  if (!LastPathContextRef.current) {
+    consoleWarn('last path context not initialized');
+  }
+  
+  const { pushPath } = useContext(LastPathContextRef.current || defaultContext());
+  
   const { pathname, searchParams } = useJukiRouter();
+  
   useEffect(() => {
     pushPath({ key, pathname, searchParams });
   }, [ key, searchParams, pathname ]);
