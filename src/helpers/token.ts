a0c089810b474a929467bc1dce1f4bf738cd8029
OscarGauss <me@oscargauss.com>
2024-02-19 18:38:10 -0400

    Fixing validate

--- src/helpers/token.ts
+++ src/helpers/token.ts
@@ -1,9 +1,15 @@
-import { validate } from 'uuid';
 import { jukiSettings } from '../config';
 import { QueryParamKey } from '../types';
 
 export const getLocalToken = () => {
   return getQueryToken() || localStorage.getItem(jukiSettings.TOKEN_NAME) || '';
+}
+
+const UUID_WITHOUT_DASHES = /^[0-9A-F]{32}$/i;
+const UUID_WITH_DASHES = /^[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}$/i;
+
+const validate = (representation: string) => {
+  return UUID_WITHOUT_DASHES.test(representation) || UUID_WITH_DASHES.test(representation)
 }
 
 export const getQueryToken = () => {
