ea2bcc6f0363c04b45d6eb253450c2c1defb976b
OscarGauss <me@oscargauss.com>
2024-02-19 18:44:44 -0400

    Fixing validate fix

--- src/helpers/token.ts
+++ src/helpers/token.ts
@@ -5,11 +5,13 @@ export const getLocalToken = () => {
   return getQueryToken() || localStorage.getItem(jukiSettings.TOKEN_NAME) || '';
 }
 
-const UUID_WITHOUT_DASHES = /^[0-9A-F]{32}$/i;
-const UUID_WITH_DASHES = /^[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}$/i;
+// const UUID_WITHOUT_DASHES = /^[0-9A-F]{32}$/i;
+// const UUID_WITH_DASHES = /^[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}$/i;
+const checkForHexRegExp = new RegExp('^[0-9a-fA-F]{24}$');
 
 const validate = (representation: string) => {
-  return UUID_WITHOUT_DASHES.test(representation) || UUID_WITH_DASHES.test(representation)
+  return representation.length === 24 && checkForHexRegExp.test(representation)
+  // return UUID_WITHOUT_DASHES.test(representation) || UUID_WITH_DASHES.test(representation)
 }
 
 export const getQueryToken = () => {
