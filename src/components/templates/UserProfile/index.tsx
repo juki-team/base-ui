d0b9d6ef62630f49ba979e16a080599d8ca5c7cd
OscarGauss <me@oscargauss.com>
2024-03-06 11:08:28 -0400

    adding lazy

--- src/components/templates/UserProfile/index.tsx
+++ src/components/templates/UserProfile/index.tsx
@@ -1,2 +1,13 @@
-export * from './UserProfile';
+import React, { lazy, Suspense } from 'react';
+import { SpinIcon } from '../../atoms';
+import { UserProfileProps } from './types';
+
+const LazyUserProfile = lazy(() => import('./UserProfile').then(module => ({ default: module.UserProfile })));
+
+export const UserProfile = (props: UserProfileProps) => (
+  <Suspense fallback={<SpinIcon />}>
+    <LazyUserProfile {...props} />
+  </Suspense>
+);
+
 export * from './types';
--- src/components/templates/UserProfileSettings/index.tsx
+++ src/components/templates/UserProfileSettings/index.tsx
@@ -1,2 +1,13 @@
-export * from './UserProfileSettings';
+import React, { lazy, Suspense } from 'react';
+import { SpinIcon } from '../../atoms';
+import { UserProfileSettingsProps } from './types';
+
+const LazyUserProfileSettings = lazy(() => import('./UserProfileSettings').then(module => ({ default: module.UserProfileSettings })));
+
+export const UserProfileSettings = (props: UserProfileSettingsProps) => (
+  <Suspense fallback={<SpinIcon />}>
+    <LazyUserProfileSettings {...props} />
+  </Suspense>
+);
+
 export * from './types';
