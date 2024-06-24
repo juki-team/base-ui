144a6730f96b909a3672592a99a65b94057486dc
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2024-06-24 09:00:35 -0400

    adding body to useFetcher

--- src/hooks/useFetcher.ts
+++ src/hooks/useFetcher.ts
@@ -7,21 +7,27 @@ const fetcherWithToken = ([ url, token ]: [ string, string ]) => {
   return authorizedRequest(url, { token, method: HTTPMethod.GET });
 };
 
-export const useFetcher = <T extends (ContentResponseType<any> | ContentsResponseType<any>)>(url?: string | null, config?: SWRConfiguration) => {
+const fetcherWithTokenWithBody = (body: string) => ([ url, token ]: [ string, string ]) => {
+  return authorizedRequest(url, { token, method: HTTPMethod.GET, body });
+};
+
+export const useFetcher = <T extends (ContentResponseType<any> | ContentsResponseType<any>)>(url?: string | null, config?: SWRConfiguration & {
+  body?: string
+}) => {
   
   const token = getLocalToken();
   
   const { data, error, mutate, isValidating, isLoading } = useSWR(
     typeof url === 'string' ? [ url, token ] : null,
-    fetcherWithToken,
+    config?.body ? fetcherWithTokenWithBody(config.body) : fetcherWithToken,
     config,
   );
   
-  return useMemo(() => ({
-    data: data ? cleanRequest<T>(data) : undefined,
+  return {
+    data: useMemo(() => data ? cleanRequest<T>(data) : undefined, [ data ]),
     error,
     isLoading,
     mutate,
     isValidating,
-  }), [ data, error, isLoading, mutate, isValidating ]);
+  };
 };
