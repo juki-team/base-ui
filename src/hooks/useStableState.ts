7a5c3d29aaf596583b994ea5e9f34b98a3fa12af
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2024-07-04 18:48:05 -0400

    validating useStableState

--- src/hooks/useStableState.ts
+++ src/hooks/useStableState.ts
@@ -1,15 +1,16 @@
+import { isObjectJson, isStringJson } from '@juki-team/commons';
 import { Dispatch, SetStateAction, useEffect, useRef, useState } from 'react';
 
 export const useStableState = <T, >(initialStableState: T): [ T, Dispatch<SetStateAction<T>> ] => {
   
   const [ state, setState ] = useState<T>(initialStableState);
   
-  const initialStableStateString = JSON.stringify(initialStableState);
-  const stateRef = useRef(JSON.stringify(state));
-  stateRef.current = JSON.stringify(state);
+  const initialStableStateString = isObjectJson(initialStableState) ? JSON.stringify(initialStableState) : '';
+  const stateRef = useRef<string>('');
+  stateRef.current = isObjectJson(state) ? JSON.stringify(state) : '';
   
   useEffect(() => {
-    if (stateRef.current !== initialStableStateString) {
+    if (stateRef.current !== initialStableStateString && isStringJson(initialStableStateString)) {
       setState(JSON.parse(initialStableStateString));
     }
   }, [ initialStableStateString ]);
