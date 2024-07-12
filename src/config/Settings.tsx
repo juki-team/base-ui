09014839af46126beb1346f9785e7e7497f38218
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2024-07-12 12:53:25 -0400

    fixing contest routes

--- src/config/Settings.tsx
+++ src/config/Settings.tsx
@@ -467,8 +467,8 @@ export class Settings {
           list() {
             return injectOrigin(`/contests`);
           },
-          view({ key, tab = ContestTab.OVERVIEW }: { key: string, tab?: ContestTab }) {
-            return injectOrigin(`/contests/${key}?tab=${tab}`);
+          view({ key, tab = ContestTab.OVERVIEW, subTab }: { key: string, tab?: ContestTab, subTab?: number }) {
+            return injectOrigin(`/contests/${key}?tab=${tab}${typeof subTab === 'number' ? '&subTab=' + subTab : ''}`);
           },
           edit({ key }: { key: string }) {
             return injectOrigin(`/contests/${key}/edit`);
