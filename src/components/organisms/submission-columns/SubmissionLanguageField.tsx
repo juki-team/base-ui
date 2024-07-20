e10f88affa83bb703c13847fe1bcbaaf87c56dd0
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2024-07-20 13:39:34 -0400

    adding LanguagesByJudge

--- src/components/organisms/submission-columns/SubmissionLanguageField.tsx
+++ src/components/organisms/submission-columns/SubmissionLanguageField.tsx
@@ -3,23 +3,25 @@ import React from 'react';
 import { SubmissionInfo } from '../../templates/Submission/SubmissionInfo';
 import { DataViewerHeadersType, Field } from '../DataViewer';
 
-export const getSubmissionLanguageHeader = (languages: {
-  [key: string]: { key: string | Judge, name: string, languages: { [key: string]: { label: string, value: string } } }
-}): DataViewerHeadersType<SubmissionSummaryListResponseDTO> => ({
+export type LanguagesByJudge = {
+  [key: string]: { key: string | Judge, name: string, languages: { [key: string]: { label: string, value: string } } },
+};
+
+export const getSubmissionLanguageHeader = (languagesByJudge: LanguagesByJudge): DataViewerHeadersType<SubmissionSummaryListResponseDTO> => ({
   head: 'language',
   index: 'languages',
   Field: ({ record: { submitId, user: { canViewSourceCode }, language, problem: { judgeKey } }, isCard }) => (
     isCard ? null :
       <Field>
         <SubmissionInfo submitId={submitId} canViewSourceCode={canViewSourceCode}>
-          <div className="jk-col extend">{languages[judgeKey]?.languages[language]?.label || language}</div>
+          <div className="jk-col extend">{languagesByJudge[judgeKey]?.languages[language]?.label || language}</div>
         </SubmissionInfo>
       </Field>
   ),
   sort: true,
   filter: {
     type: 'select',
-    options: Object.values(languages).map(({ name, languages }) => (
+    options: Object.values(languagesByJudge).map(({ name, languages }) => (
       Object.values(languages).map(({ label, value }) => ({
         label: Object.keys(languages).length === 1
           ? label
