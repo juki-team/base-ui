89dbee99d5e8afa90545de061d72c7a4f9de8657
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2024-07-20 13:36:49 -0400

    fixing getSubmissionLanguageHeader

--- src/components/organisms/submission-columns/SubmissionLanguageField.tsx
+++ src/components/organisms/submission-columns/SubmissionLanguageField.tsx
@@ -1,31 +1,38 @@
-import { SubmissionSummaryListResponseDTO } from '@juki-team/commons';
+import { Judge, SubmissionSummaryListResponseDTO } from '@juki-team/commons';
 import React from 'react';
 import { SubmissionInfo } from '../../templates/Submission/SubmissionInfo';
 import { DataViewerHeadersType, Field } from '../DataViewer';
 
 export const getSubmissionLanguageHeader = (languages: {
-  [key: string]: { label: string, value: string }
+  [key: string]: { key: string | Judge, name: string, languages: { [key: string]: { label: string, value: string } } }
 }): DataViewerHeadersType<SubmissionSummaryListResponseDTO> => ({
   head: 'language',
   index: 'languages',
-  Field: ({ record: { submitId, user: { canViewSourceCode }, language }, isCard }) => (
+  Field: ({ record: { submitId, user: { canViewSourceCode }, language, problem: { judgeKey } }, isCard }) => (
     isCard ? null :
       <Field>
         <SubmissionInfo submitId={submitId} canViewSourceCode={canViewSourceCode}>
-          <div className="jk-col extend">{languages[language]?.label || language}</div>
+          <div className="jk-col extend">{languages[judgeKey]?.languages[language]?.label || language}</div>
         </SubmissionInfo>
       </Field>
   ),
   sort: true,
   filter: {
     type: 'select',
-    options: Object.values(languages).map(language => (
-      {
-        label: language.label,
-        value: language.value,
-      }
-    )),
+    options: Object.values(languages).map(({ name, languages }) => (
+      Object.values(languages).map(({ label, value }) => ({
+        label: Object.keys(languages).length === 1
+          ? label
+          : (
+            <div className="jk-row">
+              {label}&nbsp;
+              <div className="jk-tag gray-6 tx-t">{name}</div>
+            </div>
+          ),
+        value,
+      }))
+    )).flat(),
   },
   cardPosition: 'bottom',
-  minWidth: 140,
+  minWidth: 180,
 });
