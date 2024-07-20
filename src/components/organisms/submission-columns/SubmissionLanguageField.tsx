7e94a55987966c34148417c454757b4d532c1c20
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2024-07-20 13:06:22 -0400

    fixing getSubmissionLanguageHeader

--- src/components/organisms/submission-columns/SubmissionLanguageField.tsx
+++ src/components/organisms/submission-columns/SubmissionLanguageField.tsx
@@ -1,30 +1,28 @@
-import {
-  ACCEPTED_PROGRAMMING_LANGUAGES,
-  PROGRAMMING_LANGUAGE,
-  SubmissionSummaryListResponseDTO,
-} from '@juki-team/commons';
+import { SubmissionSummaryListResponseDTO } from '@juki-team/commons';
 import React from 'react';
 import { SubmissionInfo } from '../../templates/Submission/SubmissionInfo';
 import { DataViewerHeadersType, Field } from '../DataViewer';
 
-export const getSubmissionLanguageHeader = (): DataViewerHeadersType<SubmissionSummaryListResponseDTO> => ({
+export const getSubmissionLanguageHeader = (languages: {
+  [key: string]: { label: string, value: string }
+}): DataViewerHeadersType<SubmissionSummaryListResponseDTO> => ({
   head: 'language',
   index: 'languages',
   Field: ({ record: { submitId, user: { canViewSourceCode }, language }, isCard }) => (
     isCard ? null :
       <Field>
         <SubmissionInfo submitId={submitId} canViewSourceCode={canViewSourceCode}>
-          <div className="jk-col extend">{PROGRAMMING_LANGUAGE[language]?.label || language}</div>
+          <div className="jk-col extend">{languages[language]?.label || language}</div>
         </SubmissionInfo>
       </Field>
   ),
   sort: true,
   filter: {
     type: 'select',
-    options: ACCEPTED_PROGRAMMING_LANGUAGES.map(language => (
+    options: Object.values(languages).map(language => (
       {
-        label: PROGRAMMING_LANGUAGE[language].label,
-        value: language,
+        label: language.label,
+        value: language.value,
       }
     )),
   },
