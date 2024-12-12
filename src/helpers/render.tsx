db928fb273f15e4574f35dc9704d4740872658c4
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2024-12-12 14:47:56 -0400

    fixing types

--- src/helpers/render.tsx
+++ src/helpers/render.tsx
@@ -1,12 +1,12 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, ReactNode } from 'react';
 import { ReactNodeOrFunctionP1Type, ReactNodeOrFunctionType } from '../types';
 
-export const renderReactNodeOrFunction = (content: ReactNodeOrFunctionType, key?: string | number) => {
+export const renderReactNodeOrFunction = (content: ReactNodeOrFunctionType, key?: string | number): ReactNode => {
   const reactNode = typeof content === 'function' ? content() : content;
   return key ? <Fragment key={key}>{reactNode}</Fragment> : reactNode;
 };
 
-export const renderReactNodeOrFunctionP1 = <T, >(content: ReactNodeOrFunctionP1Type<T>, prop1: T, key?: string | number) => {
+export const renderReactNodeOrFunctionP1 = <T, >(content: ReactNodeOrFunctionP1Type<T>, prop1: T, key?: string | number): ReactNode => {
   const reactNode = typeof content === 'function' ? content(prop1) : content;
   return key ? <Fragment key={key}>{reactNode}</Fragment> : reactNode;
 };
