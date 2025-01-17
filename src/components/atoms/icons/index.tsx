6c151545840b68cf30e05dd335c7c8c3b2cf7928
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2025-01-17 03:42:04 -0400

    v1.0.36

--- src/components/atoms/icons/index.tsx
+++ src/components/atoms/icons/index.tsx
@@ -1,133 +1,130 @@
-import React from 'react';
-import { classNames } from '../../../helpers';
-
-import Apps from './Apps'; // not antd
-import Appstore from './Appstore';
-import CaretLeft from './CaretLeft';
-import DoubleArrow from './DoubleArrow';
-import DragIndicator from './DragIndicator'; // not antd
-import Facebook from './Facebook';
-import FileDone from './FileDone';
-import InfoCircle from './InfoCircle';
-import Layout from './Layout';
-import MessagePlus from './MessagePlus';
-import MessageQuestion from './MessageQuestion';
-import Note from './Note';
-import NoteCode from './NoteCode'; // not antd
-import Read from './Read';
-import { IconProps } from './types';
-import Upload from './Upload';
-import User from './User';
-import ZoomIn from './ZoomIn';
-import ZoomOut from './ZoomOut';
-
-export const AppsIcon = ({ size = 'regular', className = '', ...props }: IconProps) => {
-  return (
-    <span {...props} className={classNames(className, 'jk-icon', size)}><Apps /></span>
-  );
-};
-
-export const AppstoreIcon = ({ size = 'regular', className = '', ...props }: IconProps) => {
-  return (
-    <span {...props} className={classNames(className, 'jk-icon', size)}><Appstore /></span>
-  );
-};
-
-export const CaretLeftIcon = ({ size = 'regular', className = '', ...props }: IconProps) => {
-  return (
-    <span {...props} className={classNames(className, 'jk-icon', size)}><CaretLeft /></span>
-  );
-};
-
-export const DoubleArrowIcon = ({ size = 'regular', className = '', ...props }: IconProps) => {
-  return (
-    <span {...props} className={classNames(className, 'jk-icon', size)}><DoubleArrow /></span>
-  );
-};
-
-export const DragIndicatorIcon = ({ size = 'regular', className = '', ...props }: IconProps) => {
-  return (
-    <span {...props} className={classNames(className, 'jk-icon', size)}><DragIndicator /></span>
-  );
-};
-
-export const FacebookIcon = ({ size = 'regular', className = '', ...props }: IconProps) => {
-  return (
-    <span {...props} className={classNames(className, 'jk-icon', size, 'facebook')}><Facebook /></span>
-  );
-};
-
-export const FileDoneIcon = ({ size = 'regular', className = '', ...props }: IconProps) => {
-  return (
-    <span {...props} className={classNames(className, 'jk-icon', size)}><FileDone /></span>
-  );
-};
-
-export const InfoCircleIcon = ({ size = 'regular', className = '', ...props }: IconProps) => {
-  return (
-    <span {...props} className={classNames(className, 'jk-icon', size)}><InfoCircle /></span>
-  );
-};
-
-export const LayoutIcon = ({ size = 'regular', className = '', ...props }: IconProps) => {
-  return (
-    <span {...props} className={classNames(className, 'jk-icon', size)}><Layout /></span>
-  );
-};
-
-export const MessagePlusIcon = ({ size = 'regular', className = '', ...props }: IconProps) => {
-  return (
-    <span {...props} className={classNames(className, 'jk-icon', size)}><MessagePlus /></span>
-  );
-};
-
-export const MessageQuestionIcon = ({ size = 'regular', className = '', ...props }: IconProps) => {
-  return (
-    <span {...props} className={classNames(className, 'jk-icon', size)}><MessageQuestion /></span>
-  );
-};
-
-export const NoteIcon = ({ size = 'regular', className = '', ...props }: IconProps) => {
-  return (
-    <span {...props} className={classNames(className, 'jk-icon', size)}><Note /></span>
-  );
-};
-
-export const NoteCodeIcon = ({ size = 'regular', className = '', ...props }: IconProps) => {
-  return (
-    <span {...props} className={classNames(className, 'jk-icon', size)}><NoteCode /></span>
-  );
-};
-
-export const ReadIcon = ({ size = 'regular', className = '', ...props }: IconProps) => {
-  return (
-    <span {...props} className={classNames(className, 'jk-icon', size)}><Read /></span>
-  );
-};
-
-export const UploadIcon = ({ size = 'regular', className = '', ...props }: IconProps) => {
-  return (
-    <span {...props} className={classNames(className, 'jk-icon', size)}><Upload /></span>
-  );
-};
-
-export const UserIcon = ({ size = 'regular', className = '', ...props }: IconProps) => {
-  return (
-    <span {...props} className={classNames(className, 'jk-icon', size)}><User /></span>
-  );
-};
-
-export const ZoomInIcon = ({ size = 'regular', className = '', ...props }: IconProps) => {
-  return (
-    <span {...props} className={classNames(className, 'jk-icon', size)}><ZoomIn /></span>
-  );
-};
-
-export const ZoomOutIcon = ({ size = 'regular', className = '', ...props }: IconProps) => {
-  return (
-    <span {...props} className={classNames(className, 'jk-icon', size)}><ZoomOut /></span>
-  );
-};
+// import Apps from './Apps'; // not antd
+// import Appstore from './Appstore';
+// import CaretLeft from './CaretLeft';
+// import DoubleArrow from './DoubleArrow';
+// import DragIndicator from './DragIndicator'; // not antd
+// import Facebook from './Facebook';
+// import FileDone from './FileDone';
+// import InfoCircle from './InfoCircle';
+// import Layout from './Layout';
+// import MessagePlus from './MessagePlus';
+// import MessageQuestion from './MessageQuestion';
+// import Note from './Note';
+// import NoteCode from './NoteCode'; // not antd
+// import Read from './Read';
+// import { IconProps } from './types';
+// import Upload from './Upload';
+// import User from './User';
+// import ZoomIn from './ZoomIn';
+// import ZoomOut from './ZoomOut';
+//
+// export const AppsIcon = ({ size = 'regular', className = '', ...props }: IconProps) => {
+//   return (
+//     <span {...props} className={classNames(className, 'jk-icon', size)}><Apps /></span>
+//   );
+// };
+//
+// export const AppstoreIcon = ({ size = 'regular', className = '', ...props }: IconProps) => {
+//   return (
+//     <span {...props} className={classNames(className, 'jk-icon', size)}><Appstore /></span>
+//   );
+// };
+//
+// export const CaretLeftIcon = ({ size = 'regular', className = '', ...props }: IconProps) => {
+//   return (
+//     <span {...props} className={classNames(className, 'jk-icon', size)}><CaretLeft /></span>
+//   );
+// };
+//
+// export const DoubleArrowIcon = ({ size = 'regular', className = '', ...props }: IconProps) => {
+//   return (
+//     <span {...props} className={classNames(className, 'jk-icon', size)}><DoubleArrow /></span>
+//   );
+// };
+//
+// export const DragIndicatorIcon = ({ size = 'regular', className = '', ...props }: IconProps) => {
+//   return (
+//     <span {...props} className={classNames(className, 'jk-icon', size)}><DragIndicator /></span>
+//   );
+// };
+//
+// export const FacebookIcon = ({ size = 'regular', className = '', ...props }: IconProps) => {
+//   return (
+//     <span {...props} className={classNames(className, 'jk-icon', size, 'facebook')}><Facebook /></span>
+//   );
+// };
+//
+// export const FileDoneIcon = ({ size = 'regular', className = '', ...props }: IconProps) => {
+//   return (
+//     <span {...props} className={classNames(className, 'jk-icon', size)}><FileDone /></span>
+//   );
+// };
+//
+// export const InfoCircleIcon = ({ size = 'regular', className = '', ...props }: IconProps) => {
+//   return (
+//     <span {...props} className={classNames(className, 'jk-icon', size)}><InfoCircle /></span>
+//   );
+// };
+//
+// export const LayoutIcon = ({ size = 'regular', className = '', ...props }: IconProps) => {
+//   return (
+//     <span {...props} className={classNames(className, 'jk-icon', size)}><Layout /></span>
+//   );
+// };
+//
+// export const MessagePlusIcon = ({ size = 'regular', className = '', ...props }: IconProps) => {
+//   return (
+//     <span {...props} className={classNames(className, 'jk-icon', size)}><MessagePlus /></span>
+//   );
+// };
+//
+// export const MessageQuestionIcon = ({ size = 'regular', className = '', ...props }: IconProps) => {
+//   return (
+//     <span {...props} className={classNames(className, 'jk-icon', size)}><MessageQuestion /></span>
+//   );
+// };
+//
+// export const NoteIcon = ({ size = 'regular', className = '', ...props }: IconProps) => {
+//   return (
+//     <span {...props} className={classNames(className, 'jk-icon', size)}><Note /></span>
+//   );
+// };
+//
+// export const NoteCodeIcon = ({ size = 'regular', className = '', ...props }: IconProps) => {
+//   return (
+//     <span {...props} className={classNames(className, 'jk-icon', size)}><NoteCode /></span>
+//   );
+// };
+//
+// export const ReadIcon = ({ size = 'regular', className = '', ...props }: IconProps) => {
+//   return (
+//     <span {...props} className={classNames(className, 'jk-icon', size)}><Read /></span>
+//   );
+// };
+//
+// export const UploadIcon = ({ size = 'regular', className = '', ...props }: IconProps) => {
+//   return (
+//     <span {...props} className={classNames(className, 'jk-icon', size)}><Upload /></span>
+//   );
+// };
+//
+// export const UserIcon = ({ size = 'regular', className = '', ...props }: IconProps) => {
+//   return (
+//     <span {...props} className={classNames(className, 'jk-icon', size)}><User /></span>
+//   );
+// };
+//
+// export const ZoomInIcon = ({ size = 'regular', className = '', ...props }: IconProps) => {
+//   return (
+//     <span {...props} className={classNames(className, 'jk-icon', size)}><ZoomIn /></span>
+//   );
+// };
+//
+// export const ZoomOutIcon = ({ size = 'regular', className = '', ...props }: IconProps) => {
+//   return (
+//     <span {...props} className={classNames(className, 'jk-icon', size)}><ZoomOut /></span>
+//   );
+// };
 
 export * from './google';
 export * from './signs';
