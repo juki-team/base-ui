785de257ecb930cdbcb4cfa8f750296e990f9cab
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2025-03-15 23:09:59 -0300

    upd storybook

--- src/components/mockup/MockupJukiProvider.tsx
+++ src/components/mockup/MockupJukiProvider.tsx
@@ -45,9 +45,9 @@ await i18nInstance.init({
 export const MockupJukiProvider = ({ children }: PropsWithChildren) => {
   
   // const serviceUrl = 'https://service.juki.app/api/v1';
-  const serviceUrl = 'https://api.juki.app/v2/app';
+  const serviceUrl = 'https://api.juki.app/v2';
   // const serviceUrl = 'http://localhost:3005';
-  const serviceV2Url = 'https://api.juki.app/v2/app';
+  const serviceV2Url = 'https://api.juki.app/v2';
   // const socketServiceUrl = 'wss://im7lou2on3.execute-api.us-east-1.amazonaws.com/production';
   // const socketServiceUrl = 'wss://im7lou2on3.execute-api.us-east-1.amazonaws.com/v1/';
   const socketServiceUrl = 'wss://websocket.juki.app';
