2114dc7923c9dd5cc244492a9b1433448fa30185
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2025-03-19 15:42:47 -0400

    fixing circular dependencies 3

--- src/components/atoms/Collapse/index.tsx
+++ src/components/atoms/Collapse/index.tsx
@@ -1,6 +1,6 @@
 import React, { lazy, Suspense } from 'react';
 import { JukiLoadingLayout } from '../../molecules/layouts/PawsLoadingLayout';
-import { T } from '../index';
+import { T } from '../T';
 import { CollapseProps } from './types';
 
 const LazyCollapse = lazy(() => import('./Collapse').then(module => ({ default: module.Collapse })));
