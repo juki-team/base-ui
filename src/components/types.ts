a11bf569b63675debeff87402359783e11e14bc4
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2025-04-15 19:38:37 -0400

    adding server/types

--- src/components/types.ts
+++ src/components/types.ts
@@ -2,3 +2,4 @@ export type * from './atoms/types';
 export type * from './molecules/types';
 export type * from './organisms/types';
 export type * from './templates/types';
+export type * from './server/types';
