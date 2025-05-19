e722d021d8de47f5d9f38939a666d4ea7e10bf3b
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2025-05-19 16:31:35 -0400

    adding persist useUserStore

--- src/stores/user/useUserStore.ts
+++ src/stores/user/useUserStore.ts
@@ -1,6 +1,7 @@
 import { CompanyPingType, UserPingType } from '@juki-team/commons';
 import { KeyedMutator } from 'swr';
-import { create } from 'zustand/index';
+import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
 import { EMPTY_COMPANY, EMPTY_USER } from '../../constants';
 import { DeviceType } from './types';
 
@@ -16,20 +17,34 @@ interface UserState {
   setDevice: (user: DeviceType) => void,
 }
 
-export const useUserStore = create<UserState>((set) => ({
-  user: EMPTY_USER,
-  setUser: (user) => set(state => {
-    const newUser = { ...state.user, ...user };
-    if (JSON.stringify(state.user) !== JSON.stringify(newUser)) {
-      return { user: newUser, isLoading: !newUser?.sessionId };
-    }
-    return state;
-  }),
-  company: EMPTY_COMPANY,
-  setCompany: (company) => set({ company }),
-  isLoading: true,
-  mutate: null as unknown as KeyedMutator<any>,
-  setMutate: (mutate) => set({ mutate }),
-  device: { label: '', isMobile: false, isBrowser: false, type: '', osLabel: '' },
-  setDevice: (device) => set({ device }),
-}));
+export const useUserStore = create<UserState>()(
+  persist(
+    (set, get) => ({
+      user: EMPTY_USER,
+      setUser: (user) => set(state => {
+        const newUser = { ...state.user, ...user };
+        if (JSON.stringify(state.user) !== JSON.stringify(newUser)) {
+          return { user: newUser, isLoading: !newUser?.sessionId };
+        }
+        return state;
+      }),
+      company: EMPTY_COMPANY,
+      setCompany: (company) => set({ company }),
+      isLoading: true,
+      mutate: null as unknown as KeyedMutator<any>,
+      setMutate: (mutate) => set({ mutate }),
+      device: { label: '', isMobile: false, isBrowser: false, type: '', osLabel: '' },
+      setDevice: (device) => set({ device }),
+    }),
+    {
+      name: 'jk-user-store',
+      storage: createJSONStorage(() => sessionStorage),
+      partialize: (state) => ({
+        user: state.user,
+        company: state.company,
+        isLoading: state.isLoading,
+        device: state.device,
+      }),
+    },
+  ),
+);
