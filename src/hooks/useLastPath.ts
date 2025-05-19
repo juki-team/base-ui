8197b4262d393024e5663343d1147d69d3034e43
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2025-05-19 17:36:07 -0400

    testing useTrackLastPath

--- src/hooks/useLastPath.ts
+++ src/hooks/useLastPath.ts
@@ -4,11 +4,11 @@ import { useRouterStore } from '../stores/router/useRouterStore';
 import { useStore } from './useStore';
 
 export const useTrackLastPath = (key: string) => {
-  
+  console.log('useTrackLastPath', { key });
   const pushPath = useStore(useLastPathStore, (state => state.pushPath));
-  
   const pathname = useRouterStore(state => state.pathname);
   const searchParams = useRouterStore(state => state.searchParams);
+  console.log('useTrackLastPath', { pushPath, pathname, searchParams });
   useEffect(() => {
     pushPath?.({ key, pathname, searchParams });
   }, [ key, searchParams, pathname, pushPath ]);
