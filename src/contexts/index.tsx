29ff498c70888a3e3645457d0871c7b2f511c155
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2025-05-20 00:27:22 -0400

    removing lazy JukiProviders

--- src/contexts/index.tsx
+++ src/contexts/index.tsx
@@ -1,13 +1,9 @@
-import React, { lazy, PropsWithChildren, Suspense } from 'react';
-import { JukiLoadingLayout, T } from '../components';
-import { JukiProvidersProps } from './types';
-
-const LazyJukiProviders = lazy(() => import('./JukiProviders').then(module => ({ default: module.JukiProviders })));
-
-export const JukiProviders = <T extends string | number, >(props: PropsWithChildren<JukiProvidersProps<T>>) => (
-  <Suspense fallback={<JukiLoadingLayout><T className="tt-se">loading component</T></JukiLoadingLayout>}>
-    <LazyJukiProviders {...props} />
-  </Suspense>
-);
-
+// const LazyJukiProviders = lazy(() => import('./JukiProviders').then(module => ({ default: module.JukiProviders })));
+//
+// export const JukiProviders = <T extends string | number, >(props: PropsWithChildren<JukiProvidersProps<T>>) => (
+//   <Suspense fallback={<JukiLoadingLayout><T className="tt-se">loading component</T></JukiLoadingLayout>}>
+//     <LazyJukiProviders {...props} />
+//   </Suspense>
+// );
+export * from './JukiProviders';
 export * from './JukiI18nProvider';
