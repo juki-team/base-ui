8ce94e6ea52eee4bfcdc9e5bd6aa8e4b0a12c8b8
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2025-08-27 15:09:39 -0400

    adding leet code on profile

--- src/components/templates/EditProfileModal/EditProfileModal.tsx
+++ src/components/templates/EditProfileModal/EditProfileModal.tsx
@@ -167,7 +167,12 @@ export function EditProfileModal({ user, isOpen, onClose, onSuccess }: EditProfi
             />
           </div>
           <div className="fw-bd"><T className="tt-se">nicknames from other judges</T></div>
-          {[ [ Judge.CODEFORCES, Judge.CODEFORCES_GYM ], [ Judge.UVA_ONLINE_JUDGE, Judge.CODECHEF ], [ Judge.AT_CODER, Judge.TOPCODER ], [ Judge.JV_UMSA, Judge.JUKI_JUDGE ] ]
+          {[
+            [ Judge.CODEFORCES, Judge.CODEFORCES_GYM ],
+            [ Judge.UVA_ONLINE_JUDGE, Judge.CODECHEF ],
+            [ Judge.AT_CODER, Judge.TOPCODER ], [ Judge.JV_UMSA, Judge.JUKI_JUDGE ],
+            [ Judge.LEETCODE ],
+          ]
             .map(([ judge1, judge2 ]) => (
               <div className="jk-row gap block" key={`${judge1}-${judge2}`}>
                 <JudgeInput judge={JUDGE[judge1]} user={userState} setUser={setUserState} />
