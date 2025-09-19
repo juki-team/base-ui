984011177e1d5151ee3573003e03ed38afe1bf6c
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2025-09-19 19:40:19 -0400

    fixing tool buttons z-index

--- src/components/organisms/presentation-buttons/PresentationToolButtons.tsx
+++ src/components/organisms/presentation-buttons/PresentationToolButtons.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
+import { Portal } from '../../atoms';
 import { Excalidraw } from './buttons/Excalidraw';
 import { FullscreenTimer } from './buttons/FullscreenTimer';
 import { Pointer } from './buttons/Pointer';
 
 export const PresentationToolButtons = () => {
   return (
-    <div
-      style={{
-        position: 'fixed',
-        top: 'var(--pad-sm)',
-        right: 'var(--pad-sm)',
-        zIndex: 1,
-      }}
-      className="jk-col gap stretch right"
-    >
-      <Excalidraw />
-      <Pointer />
-      <FullscreenTimer />
-    </div>
+    <Portal>
+      <div
+        style={{
+          position: 'fixed',
+          top: 'var(--pad-sm)',
+          right: 'var(--pad-sm)',
+          zIndex: 'var(--z-index-tool-buttons)',
+        }}
+        className="jk-col gap stretch right"
+      >
+        <Excalidraw />
+        <Pointer />
+        <FullscreenTimer />
+      </div>
+    </Portal>
   );
 };
