d86019fcb96ad0aad538823d20afafe787034944
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2025-09-19 20:54:07 -0400

    fixing presentation buttons

--- src/components/organisms/presentation-buttons/PresentationToolButtons.tsx
+++ src/components/organisms/presentation-buttons/PresentationToolButtons.tsx
@@ -1,10 +1,57 @@
 import React from 'react';
+import { usePageStore } from '../../../stores/page/usePageStore';
 import { Portal } from '../../atoms';
+import { Button } from '../../atoms/Button/Button';
+import { FullscreenExitIcon, FullscreenIcon } from '../../atoms/server';
 import { Excalidraw } from './buttons/Excalidraw';
 import { FullscreenTimer } from './buttons/FullscreenTimer';
 import { Pointer } from './buttons/Pointer';
 
+function enterFullscreen() {
+  if (typeof document != 'undefined') {
+    const element = document.documentElement;
+    if (element.requestFullscreen) {
+      void element.requestFullscreen();
+      // @ts-ignore
+    } else if (element.webkitRequestFullscreen) {
+      // @ts-ignore
+      element.webkitRequestFullscreen();
+      // @ts-ignore
+    } else if (element.mozRequestFullScreen) {
+      // @ts-ignore
+      element.mozRequestFullScreen();
+      // @ts-ignore
+    } else if (element.msRequestFullscreen) {
+      // @ts-ignore
+      element.msRequestFullscreen();
+    }
+  }
+}
+
+function exitFullscreen() {
+  if (typeof document != 'undefined') {
+    if (document.exitFullscreen) {
+      void document.exitFullscreen();
+      // @ts-ignore
+    } else if (document.webkitExitFullscreen) {
+      // @ts-ignore
+      document.webkitExitFullscreen();
+      // @ts-ignore
+    } else if (document.mozCancelFullScreen) {
+      // @ts-ignore
+      document.mozCancelFullScreen();
+      // @ts-ignore
+    } else if (document.msExitFullscreen) {
+      // @ts-ignore
+      document.msExitFullscreen();
+    }
+  }
+}
+
 export const PresentationToolButtons = () => {
+  
+  const isFullscreen = usePageStore(store => store.isFullscreen);
+  
   return (
     <Portal>
       <div
@@ -16,9 +63,32 @@ export const PresentationToolButtons = () => {
         }}
         className="jk-col gap stretch right"
       >
-        <Excalidraw />
-        <Pointer />
-        <FullscreenTimer />
+        {isFullscreen ? (
+          <>
+            <Excalidraw />
+            <Pointer />
+            <FullscreenTimer />
+            <div className="jk-row right opacity–hover">
+              <Button
+                tooltipContent="exit fullscreen"
+                type="light"
+                size="tiny"
+                icon={<FullscreenExitIcon />}
+                onClick={() => exitFullscreen()}
+              />
+            </div>
+          </>
+        ) : (
+          <div className="jk-row right opacity–hover">
+            <Button
+              tooltipContent="fullscreen"
+              type="light"
+              size="tiny"
+              icon={<FullscreenIcon />}
+              onClick={() => enterFullscreen()}
+            />
+          </div>
+        )}
       </div>
     </Portal>
   );
