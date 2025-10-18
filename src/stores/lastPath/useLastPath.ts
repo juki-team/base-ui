c9ff51048f91d83ff106370aee831628ca78d342
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2025-10-18 15:23:11 -0400

    removing persist on useLastPathStore

--- src/stores/lastPath/useLastPath.ts
+++ src/stores/lastPath/useLastPath.ts
@@ -1,5 +1,4 @@
 import { create } from 'zustand';
-import { createJSONStorage, persist } from 'zustand/middleware';
 import { LastPathType } from '../../contexts/JukiLastPathProvider/types';
 
 interface LastPathState {
@@ -8,41 +7,19 @@ interface LastPathState {
   setInitialLastPath: (lastPath: LastPathType, nicknameUpdated: boolean) => void,
 }
 
-export const useLastPathStore = create<LastPathState>()(
-  persist((set, get) => ({
-      lastPath: {},
-      pushPath: ({ key, pathname, searchParams }) => {
-        const newLastPath = {
-          ...get().lastPath,
-          [key]: { pathname, searchParams },
-        };
-        set({ lastPath: newLastPath });
-      },
-      setInitialLastPath: (lastPath, nicknameUpdated) => {
-        if (Object.keys(get().lastPath).length === 0 || nicknameUpdated) {
-          set({ lastPath });
-        }
-      },
-    }),
-    {
-      name: 'jk-last-path-store',
-      storage: createJSONStorage(() => sessionStorage, {
-        replacer: (_, value) => {
-          if (value instanceof URLSearchParams) {
-            return { __urlSearchParams: true, value: value.toString() };
-          }
-          return value;
-        },
-        reviver: (_, value) => {
-          // @ts-ignore
-          if (value && value.__urlSearchParams) {
-            // @ts-ignore
-            return new URLSearchParams(value.value);
-          }
-          return value;
-        },
-      }),
-      partialize: (state) => ({ lastPath: state.lastPath }),
+export const useLastPathStore = create<LastPathState>()((set, get) => ({
+    lastPath: {},
+    pushPath: ({ key, pathname, searchParams }) => {
+      const newLastPath = {
+        ...get().lastPath,
+        [key]: { pathname, searchParams },
+      };
+      set({ lastPath: newLastPath });
     },
-  ),
+    setInitialLastPath: (lastPath, nicknameUpdated) => {
+      if (Object.keys(get().lastPath).length === 0 || nicknameUpdated) {
+        set({ lastPath });
+      }
+    },
+  }),
 );
