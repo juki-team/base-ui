351dc64eccb3d1a126184108d33bb96c3a5903ee
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2025-10-17 23:25:13 -0400

    fixing JukiWebSocketManagement

--- src/stores/websocket/JukiWebSocketManagement.ts
+++ src/stores/websocket/JukiWebSocketManagement.ts
@@ -18,6 +18,16 @@ import {
   isSubscribeSenDataSsmSessionsListWebSocketEventDTO,
   isSubscribeSubmissionRunStatusWebSocketEventDTO,
   isSubscribeSubmissionsCrawlWebSocketEventDTO,
+  isUnsubscribeChatCompletionsDataWebSocketEventDTO,
+  isUnsubscribeCodeRunStatusWebSocketEventDTO,
+  isUnsubscribeContestChangesWebSocketEventDTO,
+  isUnsubscribeProblemCrawledWebSocketEventDTO,
+  isUnsubscribeSenDataEc2InstancesListWebSocketEventDTO,
+  isUnsubscribeSenDataEcsTaskDefinitionsListWebSocketEventDTO,
+  isUnsubscribeSenDataEcsTasksListWebSocketEventDTO,
+  isUnsubscribeSenDataSsmSessionsListWebSocketEventDTO,
+  isUnsubscribeSubmissionRunStatusWebSocketEventDTO,
+  isUnsubscribeSubmissionsCrawlWebSocketEventDTO,
   ObjectIdType,
   ONE_MINUTE,
   PingWebSocketEventDTO,
@@ -128,6 +138,7 @@ export class JukiWebSocketManagement {
       this.callbacks[eventKey] = new Set();
     }
     this.callbacks[eventKey].add(callbackSubscription);
+    return this.sendRaw(JSON.stringify(contentResponse('sending subscribe', event)), true);
   }
   
   send(event: WebSocketEventDTO, callbackSubscription?: (data: WebSocketResponseEventDTO) => void) {
@@ -170,7 +181,7 @@ export class JukiWebSocketManagement {
   unsubscribe(event: WebSocketEventDTO, callbackSubscription: (data: WebSocketResponseEventDTO) => void) {
     const eventKey = this.getKeyWebSocketEventDTO(event);
     this.callbacks[eventKey]?.delete(callbackSubscription);
-    return this.sendRaw(JSON.stringify(contentResponse('sending unsubscribe', event)), true);
+    return this.sendRaw(JSON.stringify(contentResponse('sending unsubscribe', this.getUnsubscribeEvent(event))), true);
   }
   
   async reconnect(): Promise<boolean> {
@@ -216,35 +227,70 @@ export class JukiWebSocketManagement {
     if (isPingWebSocketEventDTO(event)) {
       return getWebSocketResponseEventKey(WebSocketResponseEvent.PONG, event.sessionId, '*');
     }
-    if (isSubscribeCodeRunStatusWebSocketEventDTO(event)) {
+    if (isSubscribeCodeRunStatusWebSocketEventDTO(event) || isUnsubscribeCodeRunStatusWebSocketEventDTO(event)) {
       return getWebSocketResponseEventKey(WebSocketResponseEvent.CODE_RUN_STATUS_MESSAGE, event.sessionId, event.runId);
     }
-    if (isSubscribeSubmissionRunStatusWebSocketEventDTO(event)) {
+    if (isSubscribeSubmissionRunStatusWebSocketEventDTO(event) || isUnsubscribeSubmissionRunStatusWebSocketEventDTO(event)) {
       return getWebSocketResponseEventKey(WebSocketResponseEvent.SUBMISSION_RUN_STATUS_MESSAGE, event.sessionId, event.submitId);
     }
-    if (isSubscribeSenDataEcsTaskDefinitionsListWebSocketEventDTO(event)) {
+    if (isSubscribeSenDataEcsTaskDefinitionsListWebSocketEventDTO(event) || isUnsubscribeSenDataEcsTaskDefinitionsListWebSocketEventDTO(event)) {
       return getWebSocketResponseEventKey(WebSocketResponseEvent.SEND_DATA_ECS_TASK_DEFINITIONS_LIST, event.sessionId, '*');
     }
-    if (isSubscribeSenDataEcsTasksListWebSocketEventDTO(event)) {
+    if (isSubscribeSenDataEcsTasksListWebSocketEventDTO(event) || isUnsubscribeSenDataEcsTasksListWebSocketEventDTO(event)) {
       return getWebSocketResponseEventKey(WebSocketResponseEvent.SEND_DATA_ECS_TASKS_LIST, event.sessionId, '*');
     }
-    if (isSubscribeSenDataEc2InstancesListWebSocketEventDTO(event)) {
+    if (isSubscribeSenDataEc2InstancesListWebSocketEventDTO(event) || isUnsubscribeSenDataEc2InstancesListWebSocketEventDTO(event)) {
       return getWebSocketResponseEventKey(WebSocketResponseEvent.SEND_DATA_EC2_INSTANCES_LIST, event.sessionId, '*');
     }
-    if (isSubscribeSenDataSsmSessionsListWebSocketEventDTO(event)) {
+    if (isSubscribeSenDataSsmSessionsListWebSocketEventDTO(event) || isUnsubscribeSenDataSsmSessionsListWebSocketEventDTO(event)) {
       return getWebSocketResponseEventKey(WebSocketResponseEvent.SEND_DATA_SSM_SESSIONS_LIST, event.sessionId, '*');
     }
-    if (isSubscribeProblemCrawledWebSocketEventDTO(event)) {
+    if (isSubscribeProblemCrawledWebSocketEventDTO(event) || isUnsubscribeProblemCrawledWebSocketEventDTO(event)) {
       return getWebSocketResponseEventKey(WebSocketResponseEvent.PROBLEM_CRAWLED, event.sessionId, event.problemKey);
     }
-    if (isSubscribeChatCompletionsDataWebSocketEventDTO(event)) {
+    if (isSubscribeChatCompletionsDataWebSocketEventDTO(event) || isUnsubscribeChatCompletionsDataWebSocketEventDTO(event)) {
       return getWebSocketResponseEventKey(WebSocketResponseEvent.CHAT_COMPLETIONS_RESPONSE, event.sessionId, '*');
     }
-    if (isSubscribeSubmissionsCrawlWebSocketEventDTO(event)) {
+    if (isSubscribeSubmissionsCrawlWebSocketEventDTO(event) || isUnsubscribeSubmissionsCrawlWebSocketEventDTO(event)) {
       return getWebSocketResponseEventKey(WebSocketResponseEvent.SUBMISSIONS_CRAWL, event.sessionId, event.contestKey + SEPARATOR_TOKEN + event.problemKeys);
     }
-    if (isSubscribeContestChangesWebSocketEventDTO(event)) {
+    if (isSubscribeContestChangesWebSocketEventDTO(event) || isUnsubscribeContestChangesWebSocketEventDTO(event)) {
       return getWebSocketResponseEventKey(WebSocketResponseEvent.CONTEST_CHANGES, event.sessionId, event.contestKey);
+    }
+    
+    return '' as WebSocketResponseEventKey;
+  }
+  
+  private getUnsubscribeEvent(event: WebSocketEventDTO) {
+    if (isSubscribeCodeRunStatusWebSocketEventDTO(event)) {
+      return { ...event, event: WebSocketActionEvent.UNSUBSCRIBE_CODE_RUN_STATUS };
+    }
+    if (isSubscribeSubmissionRunStatusWebSocketEventDTO(event)) {
+      return { ...event, event: WebSocketActionEvent.UNSUBSCRIBE_SUBMISSION_RUN_STATUS };
+    }
+    if (isSubscribeSenDataEcsTaskDefinitionsListWebSocketEventDTO(event)) {
+      return { ...event, event: WebSocketActionEvent.UNSUBSCRIBE_SEND_DATA_ECS_TASK_DEFINITIONS_LIST };
+    }
+    if (isSubscribeSenDataEcsTasksListWebSocketEventDTO(event)) {
+      return { ...event, event: WebSocketActionEvent.UNSUBSCRIBE_SEND_DATA_ECS_TASKS_LIST };
+    }
+    if (isSubscribeSenDataEc2InstancesListWebSocketEventDTO(event)) {
+      return { ...event, event: WebSocketActionEvent.UNSUBSCRIBE_SEND_DATA_EC2_INSTANCES_LIST };
+    }
+    if (isSubscribeSenDataSsmSessionsListWebSocketEventDTO(event)) {
+      return { ...event, event: WebSocketActionEvent.UNSUBSCRIBE_SEND_DATA_SSM_SESSIONS_LIST };
+    }
+    if (isSubscribeProblemCrawledWebSocketEventDTO(event)) {
+      return { ...event, event: WebSocketActionEvent.UNSUBSCRIBE_PROBLEM_CRAWLED };
+    }
+    if (isSubscribeChatCompletionsDataWebSocketEventDTO(event)) {
+      return { ...event, event: WebSocketActionEvent.UNSUBSCRIBE_CHAT_COMPLETIONS_DATA };
+    }
+    if (isSubscribeSubmissionsCrawlWebSocketEventDTO(event)) {
+      return { ...event, event: WebSocketActionEvent.UNSUBSCRIBE_SUBMISSIONS_CRAWL };
+    }
+    if (isSubscribeContestChangesWebSocketEventDTO(event)) {
+      return { ...event, event: WebSocketActionEvent.UNSUBSCRIBE_CONTEST_CHANGES };
     }
     
     return '' as WebSocketResponseEventKey;
