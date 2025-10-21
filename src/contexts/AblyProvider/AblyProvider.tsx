45fe3132c09e3c1d28840f59a9258ccc0e5d2525
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2025-10-21 10:59:23 -0400

    fixing AblyProvider

--- src/contexts/AblyProvider/AblyProvider.tsx
+++ src/contexts/AblyProvider/AblyProvider.tsx
@@ -3,10 +3,13 @@ import {
   CHANNEL_CLIENT_SUBSCRIPTIONS,
   CHANNEL_CLIENT_USER_SESSION,
   CHANNEL_SEVER_MESSAGES,
+  cleanRequest,
+  ContentResponseType,
 } from '@juki-team/commons';
-import Ably from 'ably';
+import Ably, { TokenDetails, TokenRequest } from 'ably';
 import { AblyProvider, ChannelProvider, useChannel } from 'ably/react';
 import { useEffect, useMemo } from 'react';
+import { authorizedRequest } from '../../components/helpers';
 import { jukiApiManager } from '../../settings';
 import { useUserStore } from '../../stores/user/useUserStore';
 import { useWebsocketStore } from '../../stores/websocket/useWebsocketStore';
@@ -22,9 +25,7 @@ const WebsocketProvider = () => {
   const { channel: channelSubscription } = useChannel(CHANNEL_CLIENT_SUBSCRIPTIONS);
   const { channel: channelMessages } = useChannel(CHANNEL_SEVER_MESSAGES);
   
-  console.log('WebsocketProvider');
   useEffect(() => {
-    console.log('setChannelSubscription');
     setProps({ channelSubscription });
   }, [ channelSubscription, setProps ]);
   useEffect(() => {
@@ -40,28 +41,29 @@ const WebsocketProvider = () => {
   return null;
 };
 
+const ablyClient = new Ably.Realtime({
+  authCallback: async (_, callback) => {
+    let tokenRequest;
+    try {
+      const response = cleanRequest<ContentResponseType<TokenDetails | TokenRequest | string | null>>(await authorizedRequest(jukiApiManager.API_V2.websocket.auth().url));
+      tokenRequest = response.success ? response.content : null;
+    } catch (error) {
+      callback(error?.toString() || 'Error on authCallback', null);
+      return;
+    }
+    callback(null, tokenRequest);
+  },
+});
+
 export const JukiAblyProvider = () => {
   
   const sessionId = useUserStore(store => store.user.sessionId);
-  const client = useMemo(() => {
-    if (!sessionId) {
-      return null;
-    }
-    return new Ably.Realtime({
-      authUrl: `${jukiApiManager.API_V2.websocket.auth().url}`,
-      authHeaders: {
-        'x-juki-session-id': sessionId,
-        'x-juki-forwarded-host': window?.location?.host,
-      },
-    });
+  useEffect(() => {
+    void ablyClient.auth.authorize();
   }, [ sessionId ]);
   
-  if (!client) {
-    return null;
-  }
-  
   return (
-    <AblyProvider client={client}>
+    <AblyProvider client={ablyClient}>
       <ChannelProvider channelName={CHANNEL_CLIENT_SUBSCRIPTIONS}>
         <ChannelProvider channelName={CHANNEL_CLIENT_USER_SESSION(sessionId)}>
           <ChannelProvider channelName={CHANNEL_SEVER_MESSAGES}>
