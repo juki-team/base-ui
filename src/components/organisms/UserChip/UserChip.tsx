9c962ccb297c9ed116c8921c7b8e7779c0593d58
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2025-11-16 16:19:19 -0400

    fixing UserChip

--- src/components/organisms/UserChip/UserChip.tsx
+++ src/components/organisms/UserChip/UserChip.tsx
@@ -1,4 +1,5 @@
 import { useUIStore } from '../../../stores/ui/useUIStore';
+import { useUserStore } from '../../../stores/user/useUserStore';
 import { classNames } from '../../helpers';
 import { UserNicknameLink } from '../UserNicknameLink/UserNicknameLink';
 import type { UserChipProps } from './types';
@@ -8,6 +9,7 @@ export function UserChip(props: UserChipProps) {
   const { imageUrl, email, familyName, nickname, givenName, className, companyKey, withoutLink } = props;
   
   const { Image } = useUIStore(store => store.components);
+  const userCompanyKey = useUserStore(store => store.company.key);
   
   const onlyNickname = !givenName && !familyName && !email;
   
@@ -31,6 +33,9 @@ export function UserChip(props: UserChipProps) {
         )}
         {(!!givenName || !!familyName) && <div className="fw-lr ta-cr">{givenName} {familyName}</div>}
         {!!email && <div className="fw-lr">{email}</div>}
+        {userCompanyKey !== companyKey && (
+          <div className="jk-tag bc-hl tx-t" style={{ padding: '1px 2px' }}>{companyKey}</div>
+        )}
       </div>
     </div>
   );
