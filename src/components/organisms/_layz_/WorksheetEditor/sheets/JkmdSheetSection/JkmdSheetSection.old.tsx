acdaec12af514e08158a2af2b37e9cd9e1b46006
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2025-12-03 22:48:24 -0400

    upd react & fixing presentation-tool-buttons styles fi1

--- src/components/organisms/_layz_/WorksheetEditor/sheets/JkmdSheetSection/JkmdSheetSection.old.tsx
+++ src/components/organisms/_layz_/WorksheetEditor/sheets/JkmdSheetSection/JkmdSheetSection.old.tsx
@@ -135,7 +135,7 @@ export const JkmdSheetSection = (props: JkmdSheetSectionProps) => {
               _setContent(initialContent);
             },
           })}
-          placement="out rightTop"
+          placement="right-end"
         />
       )}
     </div>
