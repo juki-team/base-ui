3c0f124a9735105aa8c9cd69ed14bebc738490dd
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2025-12-05 16:34:42 -0400

    removing persistence on createUserStore

--- src/stores/user/useUserStore.tsx
+++ src/stores/user/useUserStore.tsx
@@ -3,53 +3,56 @@ import { createContext, type PropsWithChildren, useContext, useRef } from 'react
 import { KeyedMutator } from 'swr';
 import { v4 } from 'uuid';
 import { create, useStore as useZustandStore } from 'zustand';
-import { createJSONStorage, persist } from 'zustand/middleware';
 import { StoreApi } from 'zustand/vanilla';
 import { InitUserState, UserState } from '../../providers/JukiUserProvider/types';
 
 const createUserStore = (initState: InitUserState) => create<UserState>()(
-  persist((set, get) => {
-      const uiId = v4();
-      return {
-        uiId,
-        clientId: getClientId(initState.user.sessionId, uiId),
-        user: initState.user,
-        setUser: (user) => {
-          const current = get().user;
-          const newUser = { ...current, ...user };
-          if (JSON.stringify(current) !== JSON.stringify(newUser)) {
-            set({
-              user: newUser,
-              isLoading: !newUser.sessionId,
-              clientId: getClientId(newUser.sessionId, uiId),
-            });
-          }
-        },
-        company: initState.company,
-        setCompany: (company) => {
-          const current = get().company;
-          if (JSON.stringify(current) !== JSON.stringify(company)) {
-            set({ company });
-          }
-        },
-        isLoading: initState.isLoading,
-        mutate: null as unknown as KeyedMutator<any>,
-        setMutate: (mutate) => set({ mutate }),
-        device: { label: '', isMobile: false, isBrowser: false, type: '', osLabel: '' },
-        setDevice: (device) => set({ device }),
-      };
-    },
-    {
-      name: 'jk-user-store',
-      storage: createJSONStorage(() => sessionStorage, {}),
-      partialize: (state) => ({
-        user: state.user,
-        company: state.company,
-        isLoading: state.isLoading,
-        device: state.device,
-      }),
-    },
-  ),
+  (set, get) => {
+    const uiId = v4();
+    return {
+      uiId,
+      clientId: getClientId(initState.user.sessionId, uiId),
+      user: initState.user,
+      setUser: (user) => {
+        const current = get().user;
+        const newUser = { ...current, ...user };
+        const newClientId = getClientId(newUser.sessionId, uiId);
+        if (JSON.stringify(current) !== JSON.stringify(newUser)) {
+          set({
+            user: newUser,
+            isLoading: !newUser.sessionId,
+            clientId: newClientId,
+          });
+        } else if (get().clientId !== newClientId) {
+          set({ clientId: newClientId });
+        }
+      },
+      company: initState.company,
+      setCompany: (company) => {
+        const current = get().company;
+        if (JSON.stringify(current) !== JSON.stringify(company)) {
+          set({ company });
+        }
+      },
+      isLoading: initState.isLoading,
+      mutate: null as unknown as KeyedMutator<any>,
+      setMutate: (mutate) => set({ mutate }),
+      device: { label: '', isMobile: false, isBrowser: false, type: '', osLabel: '' },
+      setDevice: (device) => set({ device }),
+    };
+  },
+  // persis(...,
+  //   {
+  //     name: 'jk-user-store',
+  //     storage: createJSONStorage(() => sessionStorage, {}),
+  //     partialize: (state) => ({
+  //       user: state.user,
+  //       company: state.company,
+  //       isLoading: state.isLoading,
+  //         device: state.device,
+  //     }),
+  //   },
+  // ),
 );
 
 const UserStoreContext = createContext<StoreApi<UserState> | null>(null);
