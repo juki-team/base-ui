d7956eeb0f865c118fb39e53f69b1086dd5882e7
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2025-12-18 01:15:23 -0400

    adding getDateLiteral

--- src/components/helpers/date.ts
+++ src/components/helpers/date.ts
@@ -1,3 +1,6 @@
+import { MONTH_NAMES } from '@juki-team/commons';
+import { i18n } from 'i18next';
+import { DateLiteralProps } from '../atoms/DateLiteral/types';
 import { DateDisplayType, TimeDisplayType } from '../types';
 
 export const showOfDateDisplayType = (type: DateDisplayType) => {
@@ -19,4 +22,30 @@ export const showOfTimeDisplayType = (type: TimeDisplayType) => {
   const showSeconds = type.includes('seconds');
   const showMilliseconds = type.includes('milliseconds');
   return { showWeeks, showDays, showHours, showMinutes, showSeconds, showMilliseconds };
+};
+
+export const getDateLiteral = (date: Date, show: Required<DateLiteralProps>['show'], t: i18n['t']) => {
+  
+  const {
+    showYears,
+    showMonths,
+    showDays,
+    showHours,
+    showMinutes,
+    showSeconds,
+    showMilliseconds,
+  } = showOfDateDisplayType(show);
+  
+  return (
+    // withDayName && <><T>{DAY_NAMES[date.getDay()]}</T>,&nbsp;</>}
+    (showDays ? date.getDate() : '') +
+    (showMonths ? ' ' + t(MONTH_NAMES[date.getMonth()] || '') : '') +
+    (showYears ? ' ' + date.getFullYear() : '') +
+    (showHours ? (
+      ', ' + date.getHours().padStart(2) +
+      (showMinutes ? ':' + date.getMinutes().padStart(2) : '') +
+      (showSeconds ? ':' + date.getSeconds().padStart(2) : '') +
+      (showMilliseconds ? '.' + date.getMilliseconds().padStart(3) : '')
+    ) : '')
+  );
 };
--- src/components/hooks/useSubscribe.ts
+++ src/components/hooks/useSubscribe.ts
@@ -1,8 +1,9 @@
 import { WebSocketResponseEventDTO, WebSocketSubscribeEventDTO } from '@juki-team/commons';
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import { useUserStore } from '../../stores/user/useUserStore';
 import { useWebsocketStore } from '../../stores/websocket/useWebsocketStore';
 import { safeReportError } from '../helpers';
+import { useStableRef } from './useStableRef';
 import { useStableState } from './useStableState';
 
 export const useSubscribe = (event: Omit<WebSocketSubscribeEventDTO, 'clientId'>, cb: (response: WebSocketResponseEventDTO) => (void | Promise<void>), isValid = () => true) => {
@@ -11,11 +12,11 @@ export const useSubscribe = (event: Omit<WebSocketSubscribeEventDTO, 'clientId'>
   const subscribeToEvent = useWebsocketStore(store => store.subscribeToEvent);
   
   const [ stableEvent ] = useStableState(event);
-  const cbRef = useRef(cb);
-  cbRef.current = cb;
+  const cbRef = useStableRef(cb);
+  const isValidRef = useStableRef(isValid);
   
   useEffect(() => {
-    if (!isValid()) {
+    if (!isValidRef.current()) {
       return;
     }
     const event: WebSocketSubscribeEventDTO = {
@@ -30,5 +31,5 @@ export const useSubscribe = (event: Omit<WebSocketSubscribeEventDTO, 'clientId'>
         await safeReportError(error as Error, null, { message: 'Error on callback useSubscribe', data, event });
       }
     });
-  }, [ clientId, stableEvent, subscribeToEvent ]);
+  }, [ cbRef, clientId, isValidRef, stableEvent, subscribeToEvent ]);
 };
