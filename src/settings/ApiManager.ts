db4ae942bccc94817cf526190e6d20e9d1d9ed95
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2025-12-19 11:25:21 -0400

    fix user endpoints

--- src/settings/ApiManager.ts
+++ src/settings/ApiManager.ts
@@ -265,6 +265,34 @@ export class ApiManager {
           url: injectBaseUrl('user', `/session/${sessionId}`),
           method: HTTPMethod.DELETE,
         })),
+        checkData: valid<{
+          params: { companyKey: string },
+          body: {
+            users: {
+              email: string,
+              givenName: string,
+              familyName: string,
+              nickname: string,
+              password: string
+            }[]
+          }
+        }, HTTPMethod.POST>(({ params: { companyKey }, body }) => ({
+          url: injectCompany(injectBaseUrl('user', `/check-data`), companyKey),
+          method: HTTPMethod.POST,
+          body: JSON.stringify(body),
+        })),
+        clientTrack: valid<{
+          body: {
+            clientId: ClientIdType,
+            location: boolean,
+            screenshot: boolean,
+            device: boolean,
+          }
+        }, HTTPMethod.POST>(({ body }) => ({
+          url: injectBaseUrl('user', `/client-track`),
+          method: HTTPMethod.POST,
+          body: JSON.stringify(body),
+        })),
       },
       problem: {
         create: valid<
@@ -345,34 +373,6 @@ export class ApiManager {
           { body: { judgeKey: Judge, key: string } }, HTTPMethod.POST
         >(({ body }) => ({
           url: injectBaseUrl('problem', `/crawl`),
-          method: HTTPMethod.POST,
-          body: JSON.stringify(body),
-        })),
-        checkData: valid<{
-          params: { companyKey: string },
-          body: {
-            users: {
-              email: string,
-              givenName: string,
-              familyName: string,
-              nickname: string,
-              password: string
-            }[]
-          }
-        }, HTTPMethod.POST>(({ params: { companyKey }, body }) => ({
-          url: injectCompany(injectBaseUrl('user', `/check-data`), companyKey),
-          method: HTTPMethod.POST,
-          body: JSON.stringify(body),
-        })),
-        clientTrack: valid<{
-          body: {
-            clientId: ClientIdType,
-            location: boolean,
-            screenshot: boolean,
-            device: boolean,
-          }
-        }, HTTPMethod.POST>(({ body }) => ({
-          url: injectBaseUrl('user', `/client-track`),
           method: HTTPMethod.POST,
           body: JSON.stringify(body),
         })),
