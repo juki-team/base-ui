2b5e3ac6e0bf4c893e77b5c267176fbc72607841
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2026-02-11 01:54:22 -0400

    fixing JukiI18nInitializer

--- src/components/providers/JukiI18nInitializer/JukiI18nInitializer.tsx
+++ src/components/providers/JukiI18nInitializer/JukiI18nInitializer.tsx
@@ -1,19 +1,13 @@
 import { useEffect } from 'react';
 import { useI18nStore } from '../../../stores/i18n/useI18nStore';
-import { usePageStore } from '../../../stores/page/usePageStore';
 
 export const JukiI18nInitializer = () => {
   
   const i18nLoadResources = useI18nStore(state => state.loadResources);
-  const isOnline = usePageStore(store => store.isOnline);
-  const isFocus = usePageStore(store => store.isFocus);
-  const isVisible = usePageStore(store => store.isVisible);
   
   useEffect(() => {
-    if (isOnline && isFocus && isVisible) {
-      void i18nLoadResources();
-    }
-  }, [ i18nLoadResources, isOnline, isFocus, isVisible ]);
+    void i18nLoadResources();
+  }, [ i18nLoadResources ]);
   
   return null;
 };
