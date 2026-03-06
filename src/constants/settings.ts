89f642eb91103423506ee717fb37df74e23dead2
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2026-03-06 10:15:33 -0400

    fixing build

--- src/constants/settings.ts
+++ src/constants/settings.ts
@@ -2,14 +2,14 @@ import { type SWRConfiguration } from 'swr';
 
 export const JUKI_SERVICE_V2_URL =
   // @ts-expect-error vite env
-  process?.env?.NEXT_PUBLIC_JUKI_SERVICE_V2_URL || import.meta.env.VITE_JUKI_SERVICE_V2_URL || '';
+  globalThis.process?.env?.NEXT_PUBLIC_JUKI_SERVICE_V2_URL || import.meta.env.VITE_JUKI_SERVICE_V2_URL || '';
 // @ts-expect-error vite env
-export const JUKI_TOKEN_NAME = process?.env?.NEXT_PUBLIC_JUKI_TOKEN_NAME || import.meta.env.VITE_JUKI_TOKEN_NAME || '';
-export const NODE_ENV = process?.env?.NODE_ENV || 'development';
+export const JUKI_TOKEN_NAME = globalThis.process?.env?.NEXT_PUBLIC_JUKI_TOKEN_NAME || import.meta.env.VITE_JUKI_TOKEN_NAME || '';
+export const NODE_ENV = globalThis.process?.env?.NODE_ENV || 'development';
 
 export const ABLY_LOG_LEVEL =
   // @ts-expect-error vite env
-  +(process?.env?.NEXT_PUBLIC_ABLY_LOG_LEVEL || import.meta.env.VITE_ABLY_LOG_LEVEL || 0) || undefined;
+  +(globalThis.process?.env?.NEXT_PUBLIC_ABLY_LOG_LEVEL || import.meta.env.VITE_ABLY_LOG_LEVEL || 0) || undefined;
 
 export const SWR_CONFIG: SWRConfiguration = {
   revalidateOnFocus: false,
