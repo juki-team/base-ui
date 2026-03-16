b8baee9bb85419c2107c8efd945965bd59821fe2
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2026-03-16 13:00:20 -0400

    adding fumadocs styles

--- src/components/molecules/_lazy_/BarChart/index.tsx
+++ src/components/molecules/_lazy_/BarChart/index.tsx
@@ -1,14 +1,12 @@
 import { FC, lazy, Suspense } from 'react';
-import type { NameType, ValueType } from 'recharts/types/component/DefaultTooltipContent';
 import { SpinIcon } from '../../../atoms/server';
 import type { BarChartProps } from './types';
 
 export const BarChartImport = () => import('./BarChart');
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const BarChartCmp = lazy(() => BarChartImport()) as FC<BarChartProps<any, any>>;
+const BarChartCmp = lazy(() => BarChartImport()) as FC<BarChartProps>;
 
-export const BarChart = <TValue extends ValueType, TName extends NameType>(props: BarChartProps<TValue, TName>) => (
+export const BarChart = (props: BarChartProps) => (
   <Suspense fallback={<SpinIcon />}>
     <BarChartCmp {...props} />
   </Suspense>
