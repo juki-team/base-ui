1ef827c26768da67ea5f72c9573df8ae857a4236
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2026-03-16 13:02:52 -0400

    fixing build

--- src/components/molecules/_lazy_/LineChart/index.tsx
+++ src/components/molecules/_lazy_/LineChart/index.tsx
@@ -1,14 +1,12 @@
 import { FC, lazy, Suspense } from 'react';
-import type { NameType, ValueType } from 'recharts/types/component/DefaultTooltipContent';
 import { SpinIcon } from '../../../atoms/server';
 import type { LineChartProps } from './types';
 
 export const LineChartImport = () => import('./LineChart');
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const LineChartGen = lazy(() => LineChartImport()) as FC<LineChartProps<any, any>>;
+const LineChartGen = lazy(() => LineChartImport()) as FC<LineChartProps>;
 
-export const LineChart = <TValue extends ValueType, TName extends NameType>(props: LineChartProps<TValue, TName>) => (
+export const LineChart = (props: LineChartProps) => (
   <Suspense fallback={<SpinIcon />}>
     <LineChartGen {...props} />
   </Suspense>
