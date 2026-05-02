15ee750de45d00b5f328b7849c7ad99aeecadfd3
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2026-05-01 23:30:54 -0400

    fixing Tooltip breakchange v5 -> v6

--- src/components/atoms/_lazy_/Tooltip/Tooltip.tsx
+++ src/components/atoms/_lazy_/Tooltip/Tooltip.tsx
@@ -10,16 +10,16 @@ export default function Tooltip() {
       positionStrategy="fixed"
       clickable
       // disableStyleInjection
-      render={
-        ({ content, activeAnchor }) =>
-          content ? (
-            activeAnchor?.getAttribute('data-tooltip-t') === 'false' ? (
-              content
-            ) : (
-              <T className={activeAnchor?.getAttribute('data-tooltip-t-class-name') ?? 'tt-se tx-s'}>{content}</T>
-            )
-          ) : null // Relevant attribute: { || 'not set'}
-      }
+      render={({ content, activeAnchor }) => {
+        if (!content) {
+          return null;
+        }
+        if (typeof content !== 'string' || activeAnchor?.getAttribute('data-tooltip-t') === 'false') {
+          return content;
+        }
+        const className = activeAnchor?.getAttribute('data-tooltip-t-class-name') ?? 'tt-se tx-s';
+        return <T className={className}>{content}</T>;
+      }}
     />
   );
 }
